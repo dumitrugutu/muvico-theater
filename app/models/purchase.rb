5810fb95084e60122404fb8e86849f8cc7ba8065
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-02-16 13:44:09 -0600

    Adds validations

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,3 +1,8 @@
 class Purchase < ApplicationRecord
   belongs_to :movie
+
+  validates_presence_of :name, :email
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  validates_uniqueness_of :email
 end
