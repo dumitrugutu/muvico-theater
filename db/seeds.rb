30f77a5fb107e3b8acfd92fd3dc708de394dd5ab
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-02-16 09:24:16 -0600

    Adds movies seeds; adds gem faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+movies = Movie.create!([{ title: "Star Wars", description: "Something about Star Wars", start_time: Faker::Time.forward(4, :morning) }, { title: "Lord of the Rings", description: "Something about Lord of the Rings", start_time: Faker::Time.forward(5, :morning)  }, {title: "True Grit", description: "Something about True Grit", start_time: Faker::Time.forward(6, :morning) }])
