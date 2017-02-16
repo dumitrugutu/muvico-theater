2752f4ab8ddbad32d28ddc2c00142f829285311b
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-02-16 12:40:18 -0600

    Saves a purchase to the database

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,4 +3,20 @@ class PurchasesController < ApplicationController
     @movie = Movie.find(params[:movie_id])
     @purchase = Purchase.new
   end
+
+  def create
+    movie = Movie.find(params[:movie_id])
+    purchase = Purchase.new(purchase_params)
+    purchase.movie_id = movie.id
+    if purchase.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def purchase_params
+      params.require(:purchase).permit(:name, :email)
+    end
 end
