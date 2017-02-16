ea98e3eee1dc64330a9fb45a0bd1f47776e94bb2
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-02-16 16:33:30 -0600

    Adds purchases

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 movies = Movie.create!([{ title: "Star Wars", description: "Something about Star Wars", start_time: Faker::Time.forward(4, :morning), price: 9.95 }, { title: "Lord of the Rings", description: "Something about Lord of the Rings", start_time: Faker::Time.forward(5, :morning), price: 10.00  }, {title: "True Grit", description: "Something about True Grit", start_time: Faker::Time.forward(6, :morning), price: 8.50 }])
+
+50.times do
+  Purchase.create!(name: Faker::Name.name, email: Faker::Internet.email, movie_id: rand(1..3))
+end
