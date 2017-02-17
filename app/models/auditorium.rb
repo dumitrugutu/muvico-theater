010bba8e12e7ed64041063f8cb3fd28583e90256
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-02-17 14:58:42 -0600

    Creates associations

--- app/models/auditorium.rb
+++ app/models/auditorium.rb
@@ -1,3 +1,5 @@
 class Auditorium < ApplicationRecord
   validates_presence_of :name, :seat_capacity
+
+  has_many :screenings
 end
--- app/models/screening.rb
+++ app/models/screening.rb
@@ -1,2 +1,5 @@
 class Screening < ApplicationRecord
+
+  belongs_to :movie
+  belongs_to :auditorium
 end
