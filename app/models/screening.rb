3a5b07a60103a1d107260e20d6fdd40d194a005f
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-02-17 17:37:22 -0600

    Creates screening; adds custom validation for screening

--- app/models/screening.rb
+++ app/models/screening.rb
@@ -1,6 +1,19 @@
 class Screening < ApplicationRecord
+
   validates_presence_of :movie_id, :auditorium_id, :start_time
+  validate :screening_exists?
 
   belongs_to :movie
   belongs_to :auditorium
+
+  private
+    def screening_exists?
+      movie_id = self.movie_id
+      auditorium_id = self.auditorium_id
+      movie_name = self.movie.title
+      auditorium_name = self.auditorium.name
+      if Screening.exists?(movie_id: movie_id, auditorium_id: auditorium_id)
+        self.errors[:base] << "Sorry, it seems like #{movie_name} is already playing in the auditorium #{auditorium_name}"
+      end
+    end
 end
