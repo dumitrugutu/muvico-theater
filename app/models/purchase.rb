9ebde316c1d9380d32c34fabe536d84d81084bdb
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-02-17 10:04:57 -0600

    Creates custom validation for email

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -3,7 +3,22 @@ class Purchase < ApplicationRecord
   validates_presence_of :name, :email
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
-  validates_uniqueness_of :email
-  
+  # validates_uniqueness_of :email
+  validate :customer_bought_ticket?
+
   belongs_to :movie
+
+  private
+    def customer_bought_ticket?
+      self.email = email
+
+      unless !email_exists?(email)
+        self.errors[:base] << "Sorry, our records show that you have already purchased a ticket!"
+      end
+    end
+
+    def email_exists?(email)
+      return false if Purchase.where("email = ?", email).empty?
+      true
+    end
 end
